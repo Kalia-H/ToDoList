1ed7c848d6ead2cb2921b9e3b5ae421a340619f2
Kalia <hudsonkalia4@gmail.com>
2024-11-10 21:01:41 -0500

    minor updates to each script and template

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+const minusButton = document.createElement('button');
 function addIt(event) {
     event.preventDefault();
     const table = document.getElementById('table');
@@ -8,7 +9,19 @@ function addIt(event) {
     let newRow = table.insertRow();
     let newtask = newRow.insertCell(0);
     let newpriority = newRow.insertCell(1);
+    let buttonPlace = newRow.insertCell(2);
+    const minusButton = document.createElement('button');
+    minusButton.type = 'button';
+    minusButton.textContent = '-';
+    minusButton.id = 'minus';
+
+    newRow.appendChild(minusButton);
 
     newtask.textContent = enteredTask;
     newpriority.textContent = enteredPriority;
+    
 }
+
+minusButton.addEventListener('click', function() {
+    
+})
